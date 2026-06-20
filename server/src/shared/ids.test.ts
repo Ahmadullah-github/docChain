46230320cc75e1d06504fb4208d4115e51865e2f
ahmadullah Ahmadi <ahmadhmanush777@gmail.com>
2026-06-20 18:57:47 +0430

    ready

--- server/src/shared/ids.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { uuid } from "./ids";
-
-describe("uuid", () => {
-  it("returns an RFC 4122 UUID string", () => {
-    expect(uuid()).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/);
-  });
-});
-
